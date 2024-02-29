5326045a760c137f800da491fa56bc3c4435b042
Ganesh Kugaji <gneshkugaji@gmail.com>
2024-02-29 14:43:43 +0530

    Signup and Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-export default function App() {
+import { useState } from 'react'
+
+import './App.css'
+import { BrowserRouter as Router ,Route,Routes } from 'react-router-dom'
+import SignUp from './Components/Signup';
+import Login  from './Components/Login';
+
+function App() {
+  
   return (
     <>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>
-
+    <Router>
+      <Routes>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/signin' element={<Login></Login>}></Route>
+      </Routes>
+    </Router>
     </>
   )
 }
+
+export default App
